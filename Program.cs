2781f31ef8ad977d8b44f974b3cb9a5d84387d94
ahmeds <ahmeds@ms.com>
2019-12-18 10:53:45 +0200

    changed hello world

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace gitcircle
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World change made!");
         }
     }
 }
